678697026dfee5be4e1689221a7a7d59be13e1aa
Danemiok <kenk514912@gmail.com>
2026-06-04 13:44:16 +0700

    feat: add Eloquent models (Teacher, Student, Class, Subject, etc)

---
+++ app/Models/generations.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class generations extends Model
+{
+    /** @use HasFactory<\Database\Factories\GenerationsFactory> */
+    use HasFactory;
+}
