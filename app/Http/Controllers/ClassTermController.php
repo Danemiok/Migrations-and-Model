b4ed0b702811174043b80db40b840444a76605a2
Danemiok <kenk514912@gmail.com>
2026-06-04 13:44:24 +0700

    feat: add controllers for CRUD operations

---
+++ app/Http/Controllers/ClassTermController.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\ClassTerm;
+use App\Http\Requests\StoreClassTermRequest;
+use App\Http\Requests\UpdateClassTermRequest;
+
+class ClassTermController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(StoreClassTermRequest $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(ClassTerm $classTerm)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(ClassTerm $classTerm)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(UpdateClassTermRequest $request, ClassTerm $classTerm)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(ClassTerm $classTerm)
+    {
+        //
+    }
+}
---
+++ app/Http/Controllers/GenerationsController.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\generations;
+use App\Http\Requests\StoregenerationsRequest;
+use App\Http\Requests\UpdategenerationsRequest;
+
+class GenerationsController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(StoregenerationsRequest $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(generations $generations)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(generations $generations)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(UpdategenerationsRequest $request, generations $generations)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(generations $generations)
+    {
+        //
+    }
+}
