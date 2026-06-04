1fd826fd65e67b150b763853901b471000fefe7b
Danemiok <kenk514912@gmail.com>
2026-06-04 13:44:37 +0700

    chore: add model factories for testing

---
+++ database/factories/ClassModelFactory.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\ClassModel;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends Factory<ClassModel>
+ */
+class ClassModelFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+            //
+        ];
+    }
+}
